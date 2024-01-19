77d5711d7e2e6189cc6618b129346f750e389857
Pierre Gastellu <pierre.gastellu97@orange.fr>
2024-01-19 12:01:55 +0100

    SchoolParis

--- school.py
+++ school.py
@@ -1,9 +1,31 @@
 # pylint:disable=C0111,C0103
+# pylint: disable=missing-docstring, C0103
+import sqlite3
+import school
+
+conn = sqlite3.connect('data/school.sqlite')
+db = conn.cursor()
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
+    
+    query="""
+        SELECT 
+                first_name,
+	            last_name 
+        FROM students
+        WHERE birth_city=  ? 
+    """
+    
+    db.execute(query, (city, ))# , pour créer un tuple
+    results = db.fetchall()
+    
+    #liste.append(i[0]) for i in results2
+    return [x for x in results]
+    # YOUR CODE HERE
+    
 
+#print (students_from_city(db, city))
 
 # To test your code, you can **run it** before running `make`
 #   => Uncomment the following lines + run:
